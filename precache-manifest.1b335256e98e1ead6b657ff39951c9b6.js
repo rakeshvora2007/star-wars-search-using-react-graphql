c0550896315305e953b65ee7b35879bf7c194b64
Rakesh Jain <rakeshvora2007@gmail.com>
2020-01-08 20:15:32 -0500

    Updates

---
+++ precache-manifest.1b335256e98e1ead6b657ff39951c9b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22a376d468265687541722f1b23f260",
+    "url": "/star-wars-search-using-react-graphql/index.html"
+  },
+  {
+    "revision": "d64c29aa43c52e2925e3",
+    "url": "/star-wars-search-using-react-graphql/static/css/main.d576947f.chunk.css"
+  },
+  {
+    "revision": "180acd5393956c55c9cf",
+    "url": "/star-wars-search-using-react-graphql/static/js/2.1cf4144e.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/star-wars-search-using-react-graphql/static/js/2.1cf4144e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d64c29aa43c52e2925e3",
+    "url": "/star-wars-search-using-react-graphql/static/js/main.5564a73b.chunk.js"
+  },
+  {
+    "revision": "341d9dda2a45597f77f8",
+    "url": "/star-wars-search-using-react-graphql/static/js/runtime-main.71c28294.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/star-wars-search-using-react-graphql/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "9c91297e17268fbd21d698ca6e526faa",
+    "url": "/star-wars-search-using-react-graphql/static/media/Starjhol.9c91297e.ttf"
+  }
+]);
