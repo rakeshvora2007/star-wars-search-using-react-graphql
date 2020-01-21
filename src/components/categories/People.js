174750d58db1d02642a7ec5ced6ce669b40d63aa
Rakesh Jain <rakeshvora2007@gmail.com>
2020-01-20 21:58:09 -0500

    people item styles

--- src/components/categories/People.js
+++ src/components/categories/People.js
@@ -5,12 +5,18 @@ const People = ({ data }) => {
     <>
       {data && data.allPeople &&
         data.allPeople.people.map((curr, idx) => (
-          <div key={curr.id}>
+          <div key={curr.id} style={styles.menuItem}>
             <p>{curr.name}</p>
           </div>
         ))}
     </>
   );
 };
+
+const styles = {
+  menuItem: {
+    border: "1px solid green"
+  }
+}
 
 export { People };
